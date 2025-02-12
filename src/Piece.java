a9fbff38d270bf89f91fa9f910356f645c275740
Selyss <99344963+Selyss@users.noreply.github.com>
2025-02-12 17:02:45 -0500

    update

--- src/Piece.java
+++ src/Piece.java
@@ -9,15 +9,15 @@ public class Piece {
         this.attributes = attributes;
     }
 
-    public boolean isBlack() { return (attributes & 0b1000) == 0; }  // 1st bit: 1 = white, 0 = black
-    public boolean isTall() { return (attributes & 0b0100) != 0; }  // 2nd bit: 1 = tall, 0 = short
-    public boolean isRound() { return (attributes & 0b0010) != 0; } // 3rd bit: 1 = round, 0 = square
-    public boolean hasHole() { return (attributes & 0b0001) != 0; } // 4th bit: 1 = hole, 0 = solid
+    public boolean isTall() { return (attributes & 0b1000) != 0; }  // 2nd bit: 1 = tall, 0 = short
+    public boolean isBlack() { return (attributes & 0b0100) == 0; }  // 1st bit: 1 = white, 0 = black
+    public boolean hasHole() { return (attributes & 0b0010) != 0; } // 4th bit: 1 = hole, 0 = solid
+    public boolean isRound() { return (attributes & 0b0001) != 0; } // 3rd bit: 1 = round, 0 = square
 
     public String getFormattedPiece() {
         String shape = isRound() ? "o" : "x";
         String size = isTall() ? shape.toUpperCase() : shape.toLowerCase();
-        String color = isBlack() ? "\033[30m" : "\033[37m"; // Black or White
+        String color = isBlack() ? "\033[30m" : "\033[97m"; // Black or White
         String background = hasHole() ? "\033[45m" : "\033[46m"; // Magenta for hole, Cyan for solid
         return background + color + " " + size + " \033[0m";
     }
